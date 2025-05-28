c5904d6b4a6fd897e684b014580eaf63124d54c5
parksubeom <sooknise@naver.com>
2025-05-28 10:12:28 +0900

    refactor : randomLockBtn 함수 리팩토링

--- src/util/components/RandomLockBtn.jsx
+++ src/util/components/RandomLockBtn.jsx
@@ -21,13 +21,13 @@ export const RandomLockBtn = ({ setLockAudio, lockaudio,lockIndex }) => {
   const [patternLock, setPatternLock] = useRecoilState(patternlock);
   const [backgroundLock, setBackgroundLock] = useRecoilState(backgroundlock);
   const allRandomCategory = [
-    [faceLock, "얼굴"],
-    [colorLock, "컬러"],
-    [eyesLock, "눈"],
-    [mouthLock, "입"],
-    [accLock, "악세서리"],
-    [patternLock, "무늬"],
-    [backgroundLock, "배경"],
+    [faceLock,setFaceLock, "얼굴"],
+    [colorLock,setColorLock, "컬러"],
+    [eyesLock,setEyesLock, "눈"],
+    [mouthLock,setMouthLock, "입"],
+    [accLock,setAccLock, "악세서리"],
+    [patternLock,setPatternLock, "무늬"],
+    [backgroundLock,setBackgroundLock, "배경"],
   ];
   /**
    *
@@ -35,44 +35,22 @@ export const RandomLockBtn = ({ setLockAudio, lockaudio,lockIndex }) => {
    * 랜덤 또는 올랜덤 버튼을 눌러도 잠긴요소는 값이 바뀌지않는다.
    * @param {number}locktype
    */
-  const randomLockBtn = (locktype) => {
-    console.log(locktype)
-    setLockAudio(!lockaudio);
-    switch (locktype) {
-      case 0:
-        setFaceLock(!faceLock);
-        break;
-      case 1:
-        setColorLock(!colorLock);
-        break;
-      case 2:
-        setEyesLock(!eyesLock);
-        break;
-      case 3:
-        setMouthLock(!mouthLock);
-        break;
-      case 4:
-        setAccLock(!accLock);
-        break;
-      case 5:
-        setPatternLock(!patternLock);
-        break;
-      case 6:
-        setBackgroundLock(!backgroundLock);
-        break;
-      default:
-        break;
-    }
-    setTimeout(() => {
-      setLockAudio(false);
-    }, 150);
-  };
+     const randomLockBtn = (locktype) => {
+      setLockAudio(!lockaudio); // 오디오 토글
+      // 해당 locktype에 맞는 Recoil setter 함수를 찾아 상태 토글
+      const currentItem = allRandomCategory[locktype][0];
+      allRandomCategory[locktype][1](!currentItem)
+  
+      setTimeout(() => {
+        setLockAudio(false); // 오디오 재생 후 150ms 뒤 끄기
+      }, 150);
+    };
   return (
             <button
-              aria-label={allRandomCategory[lockIndex][1]+"잠금"}
+              aria-label={allRandomCategory[lockIndex][2]+"잠금"}
               type="button"
               value="button"
-              className="lockBtn_Design"
+              className={`lockBtn_Design lockBtn_${lockIndex}`}
               onClick={() => randomLockBtn(lockIndex)}
             >
               {allRandomCategory[lockIndex][0] ? (
